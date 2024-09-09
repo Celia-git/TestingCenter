a05dbdec8bd3b06c68ceb7bc8356fad358125725
Celia-git <celia.hough.programmer@gmail.com>
2024-09-09 13:06:36 -0500

    added new student tests and made changes to existing student tests

--- src/test/python/test_student.py
+++ src/test/python/test_student.py
@@ -9,23 +9,30 @@ class StudentTester(unittest.TestCase):
         super().__init__(methodName)
         self.student = Student(data_path, "A00123456")
     
-    def test_aNum(self):
-        self.assertEqual(self.student.aNum, "A00123456")
+    def test_get_aNum(self):
+        self.assertEqual(self.student.get_aNum, "A00123456")
 
-    def test_name(self):
-        self.assertEqual(self.student.name, "Finley Jones")
+    def test_get_name(self):
+        self.assertEqual(self.student.get_name, "Finley Jones")
     
-    def test_courses(self):
-        self.assertEqual(self.student.courses, [['2110', 'N30']])
+    def test_get_courses(self):
+        self.assertEqual(self.student.get_courses, [['2110', 'N30']])
     
-    def test_visits(self):
-        self.assertEqual(self.student.visits, ['04/15/24'])
+    def test_get_visits(self):
+        self.assertEqual(self.student.get_visits, ['04/15/24'])
 
-    def save_data(self):
+    def test_get_save_data(self):
         self.assertEqual(self.student.save_data(), 0)
 
     def test_has_course(self):
         self.assertEqual(self.student.has_course(['2110', 'N30']), True)
+
+    def test_load_data(self):
+        new_student = Student(data_path, "A00898989")
+        self.assertEqual(new_student.aNum, "A00898989")
+        self.assertEqual(new_student.name, "Arely Barry")
+        self.assertEqual(new_student.courses, [["1710", "N35"]])
+        self.assertEqual(new_student.visits, ["04/07/24"])
 
 def run_tests():
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
